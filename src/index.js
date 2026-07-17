165d0993e65cc374797db42d2831eddc46194fe2
yao <yao@yaodeMac-Pro.local>
2026-07-17 15:31:24 +0800

    Revert "fix: use HIGH priority input.onKeyDown to intercept before mpv"
    This reverts commit 1f97f4bb3a184b2e507f1c8d650b070aee26d5d0.

--- src/index.js
+++ src/index.js
@@ -78,14 +78,18 @@ function loadCaptureKeys() {
 }
 
 function registerCaptureKeys() {
+  const introOpts = {};
+  const outroOpts = {};
   if (captureIntroKey) {
-    input.onKeyDown(captureIntroKey, () => { captureIntro(); return true }, input.PRIORITY_HIGH);
+    const kc = input.normalizeKeyCode(captureIntroKey);
+    if (!input.getAllKeyBindings()[kc]) introOpts.keyBinding = captureIntroKey;
   }
   if (captureOutroKey) {
-    input.onKeyDown(captureOutroKey, () => { captureOutro(); return true }, input.PRIORITY_HIGH);
+    const kc = input.normalizeKeyCode(captureOutroKey);
+    if (!input.getAllKeyBindings()[kc]) outroOpts.keyBinding = captureOutroKey;
   }
-  menu.addItem(menu.item("捕获片头（Hover+按I）", () => captureIntro()));
-  menu.addItem(menu.item("捕获片尾（Hover+按O）", () => captureOutro()));
+  menu.addItem(menu.item("捕获片头", () => captureIntro(), introOpts));
+  menu.addItem(menu.item("捕获片尾", () => captureOutro(), outroOpts));
 }
 
 function toggleOverlay() {
