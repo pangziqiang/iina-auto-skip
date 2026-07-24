51b4b787ac8916c4f728006ecf3434f38a371db9
yao <yao@yaodeMac-Pro.local>
2026-07-24 23:06:23 +0800

    fix: standard titled window, revert css tricks

--- src/index.js
+++ src/index.js
@@ -75,7 +75,7 @@ function togglePanel() {
     if (!windowCreated) {
       standaloneWindow.loadFile("src/settings.html")
       registerWindowHandlers()
-      standaloneWindow.setProperty({ title: "自动跳过", resizable: false, fullSizeContentView: true, hideTitleBar: true })
+      standaloneWindow.setProperty({ title: "自动跳过", resizable: false })
       standaloneWindow.setFrame(380, 600, undefined, undefined)
       standaloneWindow.open()
       windowCreated = true
